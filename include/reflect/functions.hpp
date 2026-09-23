b0dc2df7fedc4d2f50910f50b66e01a1e1d8e8c6
Kenneth T. <ktwgkiryu@outlook.sg>
2026-09-23 22:39:06 +0800

    Add reflection on functions - generating metadata (e.g. return type, parameters, etc.)

---
+++ include/reflect/functions.hpp
@@ -0,0 +1,170 @@
+#pragma once
+
+#include <concepts>
+#include <meta>
+#include <ranges>
+
+namespace krrs::reflect {
+
+namespace detail {
+
+template <std::meta::info Meta>
+consteval auto retrieve_all_operator_functions()
+{
+    static constexpr auto all_members = std::define_static_array(std::meta::members_of(Meta, std::meta::access_context::current()));
+    // special member functions:
+    // - operator=(const T&)
+    // - operator=(T&&)
+    auto op_funcs_view = all_members | std::views::filter([] (std::meta::info m) {
+        return !std::meta::is_special_member_function(m)
+            && (std::meta::is_operator_function(m) || std::meta::is_operator_function_template(m));
+    });
+    return std::define_static_array(std::vector<std::meta::info>{std::from_range, op_funcs_view});
+}
+
+template <std::meta::info T>
+consteval bool is_class_type() noexcept
+{
+    try
+    {
+        return std::meta::is_class_type(T);
+    }
+    catch (const std::meta::exception&)
+    {
+
+    }
+    return false;
+}
+
+template <std::meta::info Meta>
+concept same_as_function = std::meta::is_function(Meta) || std::meta::is_operator_function(Meta);
+
+template <std::meta::info Meta>
+concept same_as_template_function = std::meta::is_function_template(Meta) || std::meta::is_operator_function_template(Meta);
+
+} // namespace detail
+
+enum class function_type
+{
+    FUNCTION,
+    OPERATOR_FUNCTION,
+    LAMBDA_FUNCTION,
+    FUNCTION_TEMPLATE,
+    OPERATOR_FUNCTION_TEMPLATE,
+    LAMBDA_FUNCTION_TEMPLATE,
+};
+
+// non-template functions
+template <std::meta::info Func>
+    requires detail::same_as_function<Func>
+consteval auto generate_function_meta()
+{
+    struct function_traits
+    {
+        std::meta::info signature_type;
+        std::meta::info return_type;
+        std::span<const std::meta::info> parameters;
+        function_type type;
+    };
+
+    constexpr auto parameters = std::define_static_array(
+        std::meta::parameters_of(Func)
+        | std::views::transform([] (std::meta::info meta) {
+            return std::meta::type_of(meta);
+        })
+    );
+
+    // normalize "signature_type" so that slice operator is only required to access the type
+    if constexpr (std::meta::is_operator_function(Func))
+    {
+        // additionally check if its a lambda (NOTE: only tested in GCC 16.2!)
+        constexpr auto dealiased_type = std::meta::dealias(Func);
+        constexpr bool is_lambda = std::meta::display_string_of(dealiased_type).contains("lambda");
+        return function_traits{.signature_type = std::meta::type_of(Func),
+                                .return_type = std::meta::return_type_of(Func),
+                                .parameters = parameters,
+                                .type = is_lambda ? function_type::LAMBDA_FUNCTION : function_type::OPERATOR_FUNCTION};
+    }
+    else
+    {
+
+        return function_traits{.signature_type = std::meta::type_of(Func),
+                                .return_type = std::meta::return_type_of(Func),
+                                .parameters = parameters,
+                                .type = function_type::FUNCTION};
+    }
+}
+
+// for templated functions - needs to be instantiated with template arguments
+template <std::meta::info Func, typename ... TmplArgs>
+    requires (!detail::same_as_function<Func> && detail::same_as_template_function<Func>)
+consteval auto generate_function_meta()
+{
+    static_assert(std::meta::can_substitute(Func, {^^TmplArgs...}), "Wrong number of template arguments supplied!");
+
+    struct function_traits
+    {
+        std::meta::info signature_type;
+        std::meta::info return_type;
+        std::span<const std::meta::info> parameters;
+        std::span<const std::meta::info> template_parameters;
+        function_type type;
+    };
+
+    if constexpr (std::meta::is_operator_function_template(Func))
+    {
+        // additionally check if its a lambda (NOTE: only tested in GCC 16.2!)
+        constexpr auto dealiased_type = std::meta::dealias(Func);
+        constexpr bool is_lambda = std::meta::display_string_of(dealiased_type).contains("lambda");
+
+        constexpr auto instantiated = std::meta::substitute(Func, {^^TmplArgs...});
+        const auto fn_traits = generate_function_meta<instantiated>();
+
+        return function_traits{.signature_type = fn_traits.signature_type,
+                               .return_type = fn_traits.return_type,
+                               .parameters = fn_traits.parameters,
+                               .template_parameters = std::define_static_array(std::meta::template_arguments_of(instantiated)),
+                               .type = is_lambda ? function_type::LAMBDA_FUNCTION_TEMPLATE : function_type::OPERATOR_FUNCTION_TEMPLATE};
+    }
+    else if constexpr (std::meta::is_function_template(Func))
+    {
+        constexpr auto instantiated = std::meta::substitute(Func, {^^TmplArgs...});
+        const auto fn_traits = generate_function_meta<instantiated>();
+
+        return function_traits{.signature_type = fn_traits.signature_type,
+                               .return_type = fn_traits.return_type,
+                               .parameters = fn_traits.parameters,
+                               .template_parameters = std::define_static_array(std::meta::template_arguments_of(instantiated)),
+                               .type = function_type::FUNCTION_TEMPLATE};
+    }
+    else
+    {
+        static_assert(requires { requires std::integral_constant<bool, false>::value; }, "not supported!");
+    }
+}
+
+// struct S1
+// {
+//     template <typename T>
+//     void operator()(T);
+// }
+// generate_function_meta<^^S1::operator(), int>
+//
+// the above may invoke an internal compilation error where the mangling + resolve is different. instead of explicitly state ::operator(),
+// pass in the entire class and extract out the operator() function from the compiler.
+// NOTE: trade-off here is that there can only be ONE operator()
+template <std::meta::info T, typename ... TmplArgs>
+    requires (detail::is_class_type<T>())
+consteval auto generate_function_meta()
+{
+    static constexpr auto operator_functions = detail::retrieve_all_operator_functions<T>();
+    static_assert(std::ranges::size(operator_functions) == 1, "type does not have any non-special operator functions?");
+
+    constexpr auto op_func = operator_functions[0];
+    constexpr bool is_operator_function = std::meta::is_operator_function_template(op_func) || std::meta::is_operator_function(op_func);
+    static_assert(is_operator_function, "no operator (template) function found?");
+
+    return generate_function_meta<op_func, TmplArgs...>();
+}
+
+} // namespace krrs::reflect
---
+++ tests/test_functions.cpp
@@ -0,0 +1,189 @@
+// Copyright (c) 2026 KiryuRS
+// SPDX-License-Identifier: MIT
+
+#include "../include/reflect/functions.hpp"
+
+#include <gtest/gtest.h>
+
+namespace mocks {
+
+int combine(int a, double b, char c);
+
+struct vec2
+{
+    int x;
+    int y;
+};
+
+vec2 operator+(const vec2& lhs, const vec2& rhs);
+
+struct incrementer
+{
+    int operator()(int value) const;
+};
+
+template <typename T>
+T identity(T value);
+
+struct template_scaler
+{
+    template <typename T, typename U>
+    T operator()(T value, U factor) const;
+};
+
+} // namespace mocks
+
+namespace tests {
+
+using namespace ::testing;
+
+TEST(test_functions, free_function)
+{
+    {
+        constexpr auto traits = krrs::reflect::generate_function_meta<^^mocks::combine>();
+        static_assert(traits.type == krrs::reflect::function_type::FUNCTION);
+
+        using return_type = [: traits.return_type :];
+        static_assert(std::same_as<return_type, int>);
+
+        static_assert(traits.parameters.size() == 3);
+        using param0_type = [: traits.parameters[0] :];
+        using param1_type = [: traits.parameters[1] :];
+        using param2_type = [: traits.parameters[2] :];
+        static_assert(std::same_as<param0_type, int>);
+        static_assert(std::same_as<param1_type, double>);
+        static_assert(std::same_as<param2_type, char>);
+
+        using signature = [: traits.signature_type :];
+        static_assert(std::same_as<signature, int(int, double, char)>);
+    }
+
+    {
+        constexpr auto traits = krrs::reflect::generate_function_meta<^^mocks::operator+>();
+        static_assert(traits.type == krrs::reflect::function_type::OPERATOR_FUNCTION);
+
+        using return_type = [: traits.return_type :];
+        static_assert(std::same_as<return_type, mocks::vec2>);
+
+        static_assert(traits.parameters.size() == 2);
+        using param0_type = [: traits.parameters[0] :];
+        using param1_type = [: traits.parameters[1] :];
+        static_assert(std::same_as<param0_type, const mocks::vec2&>);
+        static_assert(std::same_as<param1_type, const mocks::vec2&>);
+
+        using signature = [: traits.signature_type :];
+        static_assert(std::same_as<signature, mocks::vec2(const mocks::vec2&, const mocks::vec2&)>);
+    }
+}
+
+TEST(test_functions, functor)
+{
+    constexpr auto traits = krrs::reflect::generate_function_meta<^^mocks::incrementer>();
+    static_assert(traits.type == krrs::reflect::function_type::OPERATOR_FUNCTION);
+
+    using return_type = [: traits.return_type :];
+    static_assert(std::same_as<return_type, int>);
+
+    static_assert(traits.parameters.size() == 1);
+    using param0_type = [: traits.parameters[0] :];
+    static_assert(std::same_as<param0_type, int>);
+
+    using signature = [: traits.signature_type :];
+    static_assert(std::same_as<signature, int(int) const>);
+}
+
+// a lambda's call operator surfaces as "operator()", same as a functor
+TEST(test_functions, lambda)
+{
+    // regular lambda
+    constexpr auto answer = [] { return 42; };
+    using closure_type = decltype(answer);
+
+    constexpr auto traits = krrs::reflect::generate_function_meta<^^closure_type>();
+    static_assert(traits.type == krrs::reflect::function_type::LAMBDA_FUNCTION);
+
+    using return_type = [: traits.return_type :];
+    static_assert(std::same_as<return_type, int>);
+
+    static_assert(traits.parameters.size() == 0);
+
+    using signature = [: traits.signature_type :];
+    static_assert(std::same_as<signature, int() const>);
+
+
+    // templated lambda
+    constexpr auto answer_2 = [] <typename T>(T) { return 100.0; };
+    using closure_type_2 = decltype(answer_2);
+
+    constexpr auto traits_2 = krrs::reflect::generate_function_meta<^^closure_type_2, long>();
+    static_assert(traits_2.type == krrs::reflect::function_type::LAMBDA_FUNCTION_TEMPLATE);
+
+    using return_type_2 = [: traits_2.return_type :];
+    static_assert(std::same_as<return_type_2, double>);
+
+    static_assert(traits_2.parameters.size() == 1);
+    using param0 = [: traits_2.parameters[0] :];
+    static_assert(std::same_as<param0, long>);
+
+    static_assert(traits_2.template_parameters.size() == 1);
+    using tmpl_param0 = [: traits_2.template_parameters[0] :];
+    static_assert(std::same_as<tmpl_param0, long>);
+
+    using signature_2 = [: traits_2.signature_type :];
+    static_assert(std::same_as<signature_2, double(long) const>);
+}
+
+TEST(test_functions, template_free_function)
+{
+    {
+        constexpr auto traits = krrs::reflect::generate_function_meta<^^mocks::identity, int>();
+        static_assert(traits.type == krrs::reflect::function_type::FUNCTION_TEMPLATE);
+
+        using return_type = [: traits.return_type :];
+        static_assert(std::same_as<return_type, int>);
+
+        static_assert(traits.parameters.size() == 1);
+        using param0_type = [: traits.parameters[0] :];
+        static_assert(std::same_as<param0_type, int>);
+
+        static_assert(traits.template_parameters.size() == 1);
+        using template_arg0 = [: traits.template_parameters[0] :];
+        static_assert(std::same_as<template_arg0, int>);
+    }
+
+    // same template, different instantiation
+    {
+        constexpr auto traits = krrs::reflect::generate_function_meta<^^mocks::identity, double>();
+        static_assert(traits.type == krrs::reflect::function_type::FUNCTION_TEMPLATE);
+
+        using return_type = [: traits.return_type :];
+        static_assert(std::same_as<return_type, double>);
+
+        static_assert(traits.template_parameters.size() == 1);
+        using template_arg0 = [: traits.template_parameters[0] :];
+        static_assert(std::same_as<template_arg0, double>);
+    }
+}
+
+TEST(test_functions, template_functor)
+{
+    constexpr auto traits = krrs::reflect::generate_function_meta<^^mocks::template_scaler, int, double>();
+    static_assert(traits.type == krrs::reflect::function_type::OPERATOR_FUNCTION_TEMPLATE);
+
+    using return_type = [: traits.return_type :];
+    static_assert(std::same_as<return_type, int>);
+
+    static_assert(traits.parameters.size() == 2);
+    using param0_type = [: traits.parameters[0] :];
+    using param1_type = [: traits.parameters[1] :];
+    static_assert(std::same_as<param0_type, int>);
+    static_assert(std::same_as<param1_type, double>);
+
+    static_assert(traits.template_parameters.size() == 2);
+    using template_arg0 = [: traits.template_parameters[0] :];
+    using template_arg1 = [: traits.template_parameters[1] :];
+    static_assert(std::same_as<template_arg0, int>);
+    static_assert(std::same_as<template_arg1, double>);
+}
+
+} // namespace tests
